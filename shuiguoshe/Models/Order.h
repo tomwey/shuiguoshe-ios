dcbcf940630d8bb769b50cebd8b972fcf2985285
tomwey <tomwey@163.com>
2015-03-11 09:48:22 +0800

    add alipay sdk

--- shuiguoshe/Models/Order.h
+++ shuiguoshe/Models/Order.h
@@ -8,6 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, PaymentType) {
+    PaymentTypeAlipay = 1, // 在线支付
+    PaymentTypeCash = 2, // 现金支付
+};
+
 @interface Order : NSObject
 
 @property (nonatomic, assign) NSInteger oid;
@@ -21,6 +26,30 @@
 @property (nonatomic, copy) NSString* deliveredAt;
 
 @property (nonatomic, assign) float totalPrice;
+
+@property (nonatomic, assign) PaymentType _paymentType;
+
+// 在线支付相关的属性
+@property (nonatomic, copy) NSString* partner;
+@property (nonatomic, copy) NSString* seller;
+@property (nonatomic, copy) NSString* privateKey;
+
+@property (nonatomic, copy) NSString* orderNo;
+
+@property (nonatomic, copy) NSString* productName;
+@property (nonatomic, copy) NSString* productDescription;
+
+@property (nonatomic, copy) NSString* totalFee;
+
+@property (nonatomic, copy) NSString* notifyUrl;
+
+@property (nonatomic, copy) NSString* service;
+@property (nonatomic, copy) NSString* paymentType;
+@property (nonatomic, copy) NSString* inputCharset;
+@property (nonatomic, copy) NSString* itBPay;
+@property (nonatomic, copy) NSString* showUrl;
+
+@property (nonatomic, copy) NSString* appScheme;
 
 - (id)initWithDictionary:(NSDictionary *)jsonObj;
 
--- shuiguoshe/Models/Order.m
+++ shuiguoshe/Models/Order.m
@@ -20,6 +20,27 @@
         self.deliveredAt = [jsonObj objectForKey:@"delivered_at"];
         
         self.totalPrice = [[jsonObj objectForKey:@"total_price"] floatValue];
+        
+        self._paymentType = [[jsonObj objectForKey:@"_payment_type"] integerValue];
+        
+        // 在线支付相关
+        self.partner = [jsonObj objectForKey:@"partner"];
+        self.seller = [jsonObj objectForKey:@"seller_id"];
+        self.privateKey = [jsonObj objectForKey:@"private_key"];
+        self.orderNo = [jsonObj objectForKey:@"order_no"];
+        self.productName = [jsonObj objectForKey:@"subject"];
+        self.productDescription = [jsonObj objectForKey:@"body"];
+        self.totalFee = [jsonObj objectForKey:@"total_fee"];
+        self.notifyUrl = [jsonObj objectForKey:@"notify_url"];
+        
+        self.service = [jsonObj objectForKey:@"service"];
+        self.paymentType = [jsonObj objectForKey:@"payment_type"];
+        self.inputCharset = [jsonObj objectForKey:@"_input_charset"];
+        self.itBPay = [jsonObj objectForKey:@"it_b_pay"];
+        self.showUrl = [jsonObj objectForKey:@"show_url"];
+        
+        self.appScheme = @"alipay-shuiguoshe";
+        
     }
     return self;
 }
@@ -31,7 +52,74 @@
     self.orderedAt = nil;
     self.deliveredAt = nil;
     
+    self.partner = nil;
+    self.seller = nil;
+    self.privateKey = nil;
+    
+    self.orderNo = nil;
+    
+    self.productName = nil;
+    self.productDescription = nil;
+    
+    self.totalFee = nil;
+    
+    self.notifyUrl = nil;
+    
+    self.service = nil;
+    self.paymentType = nil;
+    self.inputCharset = nil;
+    self.itBPay = nil;
+    self.showUrl = nil;
+    
+    self.appScheme = nil;
+    
     [super dealloc];
+}
+
+- (NSString *)description
+{
+    NSMutableString * discription = [NSMutableString string];
+    if (self.partner) {
+        [discription appendFormat:@"partner=\"%@\"", self.partner];
+    }
+    
+    if (self.seller) {
+        [discription appendFormat:@"&seller_id=\"%@\"", self.seller];
+    }
+    if (self.orderNo) {
+        [discription appendFormat:@"&out_trade_no=\"%@\"", self.orderNo];
+    }
+    if (self.productName) {
+        [discription appendFormat:@"&subject=\"%@\"", self.productName];
+    }
+    
+    if (self.productDescription) {
+        [discription appendFormat:@"&body=\"%@\"", self.productDescription];
+    }
+    if (self.totalFee) {
+        [discription appendFormat:@"&total_fee=\"%@\"", self.totalFee];
+    }
+    if (self.notifyUrl) {
+        [discription appendFormat:@"&notify_url=\"%@\"", self.notifyUrl];
+    }
+    
+    if (self.service) {
+        [discription appendFormat:@"&service=\"%@\"",self.service];//mobile.securitypay.pay
+    }
+    if (self.paymentType) {
+        [discription appendFormat:@"&payment_type=\"%@\"",self.paymentType];//1
+    }
+    
+    if (self.inputCharset) {
+        [discription appendFormat:@"&_input_charset=\"%@\"",self.inputCharset];//utf-8
+    }
+    if (self.itBPay) {
+        [discription appendFormat:@"&it_b_pay=\"%@\"",self.itBPay];//30m
+    }
+    if (self.showUrl) {
+        [discription appendFormat:@"&show_url=\"%@\"",self.showUrl];//m.alipay.com
+    }
+    return discription;
 }
 
 @end
